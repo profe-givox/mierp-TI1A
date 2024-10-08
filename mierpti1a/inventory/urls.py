8d88148c5d041147463bff9b0f454661386355a8
EduardoJCM <98174532+EduardoJCM@users.noreply.github.com>
2024-10-08 12:51:48 -0600

    Add files via upload

---
+++ mierpti1a/inventory/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import CustomLoginView, product_form, editar_producto, eliminar_producto, ventas, pedidos, nuevo_pedido
+from .views import editar_pedido
+from .views import eliminar_pedido
+from .views import ver_productos
+
+urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('productos/agregar/', product_form, name='product_form'),
+    path('productos/editar/', editar_producto, name='editar_producto'),
+    path('productos/eliminar/', eliminar_producto, name='eliminar_producto'),
+    path('productos/ventas/', ventas, name='ventas'),
+    path('productos/pedidos/', pedidos, name='pedidos'),
+    path('pedido/nuevo/', nuevo_pedido, name='nuevo_pedido'),
+    path('pedido/editar/', editar_pedido, name='editar_pedido'), 
+    path('pedido/eliminar/', eliminar_pedido, name='eliminar_pedido'),
+path('productos/', ver_productos, name='ver_productos'),
+]
