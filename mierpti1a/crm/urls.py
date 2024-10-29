327b32412c34db624427664883c91b2b9c389c24
Agustín Balcázar <agus.b.reyes@gmail.com>
2024-10-29 17:06:41 -0600

    Avance en las views home y faqs, además se añadio una ficture temporal para tener datos de prueba

--- mierpti1a/crm/urls.py
+++ mierpti1a/crm/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name =  "home"),
-    path('faqs/', views.faqs, name='faqs'),
+    path('/faqs', views.faqs, name='faqs'),
+    path('/comunity', views.comunity, name='comunity'),
 ]
