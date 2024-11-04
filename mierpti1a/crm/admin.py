84748d1bf55d615ef87642cda3bf6b797c8064a6
Agustín Balcázar <agus.b.reyes@gmail.com>
2024-11-04 14:45:08 -0600

    Nuevos modelos y vistas principales en /crm

--- mierpti1a/crm/admin.py
+++ mierpti1a/crm/admin.py
@@ -2,24 +2,25 @@ from django.contrib import admin
 
 from crm.models import User, FAQArticle, SupportAgent, SupportCategory, SupportChannel, Ticket, TicketComment
 
-# class ChoiceInline(admin.TabularInline):
-#     model = Choice
-#     extra = 1
+class TicketCommentInline(admin.TabularInline):
+    model = TicketComment
+    extra = 1
 
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = ["question_text", "pub_date", "was_published_recently"]
-#     fieldsets = [
-#         ("General information", {"fields": ["question_text"]}),
-#         ("Date information", {"fields": ["pub_date"]}),
-#     ] 
-#     inlines = [ChoiceInline]
-#     list_filter = ["pub_date"]
-#     search_fields = ["question_text"]
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ["title", "status", "created_at", "updated_at"]
+    fieldsets = [
+        ("General information", {"fields": ["title", "status"]}),
+    ] 
+    inlines = [TicketCommentInline]
+    list_filter = ["created_at"]
+    search_fields = ["title", "status"]
 
 
 # Register your models here.
-admin.site.register(User)
-admin.site.register(SupportAgent) 
+#admin.site.register(User)
+#admin.site.register(SupportAgent) 
 admin.site.register(FAQArticle) 
 admin.site.register(SupportCategory) 
-admin.site.register(SupportChannel) 
+admin.site.register(SupportChannel)
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(TicketComment) 
