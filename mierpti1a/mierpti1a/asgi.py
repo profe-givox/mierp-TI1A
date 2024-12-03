794ea25fa4609c20e9bff041d9748754b6353886
Agustín Balcázar Reyes <98267769+agus-br@users.noreply.github.com>
2024-12-02 19:05:06 -0600

    Update asgi.py

--- mierpti1a/mierpti1a/asgi.py
+++ mierpti1a/mierpti1a/asgi.py
@@ -1,16 +1,20 @@
-"""
-ASGI config for mierpti1a project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mierpti1a.settings')
 
-application = get_asgi_application()
+
+django_asgi_app = get_asgi_application()
+
+from crm import routing
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+    ),
+})
